05b0d72a46b2dc409f09202156b5391b5dee1473
Development <Development@fractionalaeco.com>
2026-06-12 00:47:15 -0400

    fix: transpile react-turnstile package for Next.js

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  transpilePackages: ['@marsidev/react-turnstile'],
 };
 
 export default nextConfig;
